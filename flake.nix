f5bb3556e10e589e6c8c66eafb68721b250af790
Tom Bereknyei <tomberek@gmail.com>
2021-08-25 02:37:43 -0400

    norm
    norm
    
    norm
    
    PRs and flakes
    
    PRs and flakes

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
-  {
+    {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
-    hydraJobs."tester2" = self.defaultPackage;
-    hydraJobs."tester" = self.defaultPackage;
-    hydraJobs."tester-readme" = pkgs.runCommand "readme" {} ''
-      mkdir -p $out/nix-support
-      echo "# A readme" > $out/readme.md
-      echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
+      hydraJobs."tester2" = self.defaultPackage;
+      hydraJobs."tester" = self.defaultPackage;
+      hydraJobs."tester-readme" = pkgs.runCommand "readme" { } ''
+        mkdir -p $out/nix-support
+        echo "# A readme" > $out/readme.md
+        echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
       '';
-  };
+    };
 }
