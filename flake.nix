fe4c487a5310a3e4453d82b84bab08f23330549b
Tom Bereknyei <tomberek@gmail.com>
2021-08-25 03:51:41 -0400

    bump flake

--- flake.nix
+++ flake.nix
@@ -6,13 +6,14 @@
     in
     {
 
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.tree;
 
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.tree;
 
       hydraJobs."tester2" = self.defaultPackage;
       hydraJobs."tester" = self.defaultPackage;
       hydraJobs."tester-readme" = pkgs.runCommand "readme" { } ''
+        echo hello worl
         mkdir -p $out/nix-support
         echo "# A readme" > $out/readme.md
         echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
