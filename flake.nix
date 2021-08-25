bbe48bd72a3703042776072cb98498c394d4ae96
Tom Bereknyei <tomberek@gmail.com>
2021-08-25 00:50:59 -0400

    docs

--- flake.nix
+++ flake.nix
@@ -12,5 +12,10 @@
 
     hydraJobs."tester2" = self.defaultPackage;
     hydraJobs."tester" = self.defaultPackage;
+    hydraJobs."tester-readme" = pkgs.runCommand "readme" {} ''
+      mkdir -p $out/nix-support
+      echo "# A readme" > $out/readme.md
+      echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
+      '';
   };
 }
